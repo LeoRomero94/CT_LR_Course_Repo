6a707411582634e09799e1d696a351e534b7975c
Drake <Drake@DESKTOP-MG0JHFO>
2021-01-23 15:25:54 -0300

    Creation of "DataTypes.java" class file.

---
+++ Automation_Starter_Class/src/main/java/com/java/DataTypes.java
@@ -0,0 +1,24 @@
+package com.java;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			byte variableByte = 126;
+			
+			System.out.println(variableByte);
+			
+			double peso=25.7;
+			System.out.println("El peso actual es: "+peso);
+			
+			char letra1= '\u0048';
+			
+			letra1='\u0039';
+			
+					System.out.println("Esta letra es: "+letra1);
+					
+					String frase="Hola";
+							System.out.println(frase);
+	}
+
+}
